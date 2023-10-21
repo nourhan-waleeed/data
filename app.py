f86d7de4e078d298f7a04ed43c68f7d2abd41f78
nourhan-waleeed <83437486+nourhan-waleeed@users.noreply.github.com>
2023-10-21 18:48:12 +0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import dash
+from dash import Dash, dcc, Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output  # Import the necessary classes
